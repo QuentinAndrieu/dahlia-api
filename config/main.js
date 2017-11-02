4b6cc109b5b336988926cd085f5a069811e380a1
Andrieu Quentin <quentinandrieumail@gmail.com>
2017-11-02 02:00:56 +0100

    change db

--- config/main.js
+++ config/main.js
@@ -1,5 +1,5 @@
 module.exports = {
     'secret': 'ultrasecretkey',
     // 'database': 'mongodb://localhost/Dahliadb'
-    'database': 'mongodb://heroku_7fxxxqvl:ffc4bt3ei2hd0c5bhes734mk4q@ds241895.mlab.com:41895/Dahliadb'
+    'database': 'mongodb://heroku_7fxxxqvl:ffc4bt3ei2hd0c5bhes734mk4q@ds241895.mlab.com:41895/heroku_7fxxxqvl'
 };
