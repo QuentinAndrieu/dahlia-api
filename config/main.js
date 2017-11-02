d2717b28e94a351104319ed1baa1cc0e77bf4749
Andrieu Quentin <quentinandrieumail@gmail.com>
2017-11-02 02:22:41 +0100

    optimisation

--- config/main.js
+++ config/main.js
@@ -1,5 +1,4 @@
 module.exports = {
     'secret': 'ultrasecretkey',
-    // 'database': 'mongodb://localhost/Dahliadb'
-    'database': 'mongodb://heroku_7fxxxqvl:ffc4bt3ei2hd0c5bhes734mk4q@ds241895.mlab.com:41895/heroku_7fxxxqvl'
+    'database': process.env.MONGODB_URI | 'mongodb://localhost/Dahliadb'
 };
