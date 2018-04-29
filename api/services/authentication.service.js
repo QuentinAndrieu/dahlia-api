e487608e24086991a2d6d03518f211b5f21a814e
Andrieu Quentin <quentinandrieumail@gmail.com>
2018-04-29 15:18:29 +1200

    Manage duplicate key

--- api/services/authentication.service.js
+++ api/services/authentication.service.js
@@ -5,7 +5,9 @@ import config from '../config/main';
 import jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/user.interface';
 
+
 let User = mongoose.model('User');
+
 
 exports.register = (user: IUser, role: string): Promise<IUser> => {
     return new Promise((resolve, reject) => {
@@ -15,17 +17,26 @@ exports.register = (user: IUser, role: string): Promise<IUser> => {
             winston.error('REGISTER_REJECTED', 'Missing mail or password');
             reject('Please enter mail and password.');
         } else {
-            let new_user = new User(user);
-            new_user.role = role;
-
-            // Attempt to save the user
-            new_user.save((err, user) => {
-                if (err) {
-                    winston.error('REGISTER_REJECTED', err);
-                    reject(err);
+            User.find({
+                mail: user.mail
+            }, (err, user) => {
+                if(user){
+                    winston.error('REGISTER_REJECTED', 'User already exist');
+                    reject('User already exist');
                 } else {
-                    winston.info('REGISTER_FULLFILED');
-                    resolve(user);
+                    let new_user = new User(user);
+                    new_user.role = role;
+        
+                    // Attempt to save the user
+                    new_user.save((err, user) => {
+                        if (err) {
+                            winston.error('REGISTER_REJECTED', err);
+                            reject(err);
+                        } else {
+                            winston.info('REGISTER_FULLFILED');
+                            resolve(user);
+                        }
+                    });
                 }
             });
         }
